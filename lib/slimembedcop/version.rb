7fc4e3ffcb82ab7492e1e8ef805c9b5a01c56f63
ydah <13041216+ydah@users.noreply.github.com>
2023-07-21 14:28:04 +0900

    Initial commit

---
+++ lib/slimembedcop/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Slimembedcop
+  VERSION = "0.1.0"
+end
---
+++ spec/slimembedcop_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Slimembedcop do
+  it "has a version number" do
+    expect(Slimembedcop::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
