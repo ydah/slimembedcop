78bc239d0bdc251e39f97072b4c9dfdaa0af7967
ydah <13041216+ydah@users.noreply.github.com>
2023-09-15 19:22:23 +0900

    Add test for config_ruby_offense_collector

---
+++ spec/slimembedcop/ruby_offense_collector_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+RSpec.describe Slimembedcop::RubyOffenseCollector do
+  describe "#run" do
+    subject { described_class.new("dummy.slim", config, source, false).run }
+
+    let(:option) { Slimembedcop::Option.new("") }
+    let(:config) { Slimembedcop::ConfigGenerator.new(option).run }
+    let(:source) do
+      <<~RUBY
+        "a"
+      RUBY
+    end
+
+    context "when valid condition" do
+      it "returns expected offenses" do
+        expect(subject).not_to be_empty
+      end
+    end
+
+    context "when rubocop:todo comment" do
+      let(:source) do
+        <<~RUBY
+          "a" # rubocop:todo Style/StringLiterals
+        RUBY
+      end
+
+      it "excludes disabled offenses" do
+        expect(subject).to be_empty
+      end
+    end
+
+    context "when rubocop:disable comment" do
+      let(:source) do
+        <<~RUBY
+          "a" # rubocop:disable Style/StringLiterals
+        RUBY
+      end
+
+      it "excludes disabled offenses" do
+        expect(subject).to be_empty
+      end
+    end
+
+    context "when rubocop:disable comment with missing cop enable directive" do
+      let(:source) do
+        <<~RUBY
+          # rubocop:disable Style/StringLiterals
+          "a"
+        RUBY
+      end
+
+      it "excludes disabled offenses" do
+        expect(subject.size).to eq 1
+        expect(subject.first.cop_name).to eq "Lint/MissingCopEnableDirective"
+      end
+    end
+  end
+end
